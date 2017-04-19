3353ba9beceb1b041416148c388cee838a30020a
Pau Riba Fiérrez <ragnos.91@gmail.com>
2017-04-19 21:43:34 +0200

    Main structure

---
+++ main.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+import argparse
+
+import torch
+
+# TODO Argument parser
+
+# TODO Load data
+
+# TODO Define model and optimizer
+
+# TODO Train function
+
+# TODO Test function
+
+# TODO Epoch for loop
