56b2a614134a461a9464fc72c5a0c7e46df87c1b
Pau Riba Fiérrez <ragnos.91@gmail.com>
2017-04-19 22:38:24 +0200

    Add argument parser function

--- main.py
+++ main.py
@@ -4,14 +4,41 @@ import argparse
 
 import torch
 
-# TODO Argument parser
+## Argument parser
 
-# TODO Load data
+parser = argparse.ArgumentParser(description='Neural message passing')
 
-# TODO Define model and optimizer
+parser.add_argument('--dataset', default='MUTAG', help='MUTAG')
+parser.add_argument('--datasetPath', default='./db/mutag', help='dataset path')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='Input batch size for training (default: 64)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='Enables CUDA training')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='Number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='Learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='How many batches to wait before logging training status')
 
-# TODO Train function
+args = parser.parse_args()
 
-# TODO Test function
+# Check if CUDA is enabled
+args.cuda = not args.no_cuda and torch.cuda.is_available()
 
-# TODO Epoch for loop
+## TODO Load data
+
+## TODO Define model and optimizer
+
+## TODO Train function
+def train(epoch):
+	# TODO
+
+## TODO Test function
+def test(epoch):
+	# TODO
+
+## TODO Epoch for loop
+for epoch in range(1, args.epochs + 1):
