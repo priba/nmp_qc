7778632eea61f89ac064e6226d21ff4436b20f9f
Pau Riba <priba@cvc.uab.cat>
2017-04-20 14:25:56 +0200

    Enhance comments and TODO

--- data/download.py
+++ data/download.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 """
     download.py: Download the needed datasets.
 """
@@ -9,13 +10,8 @@ __email__ = "priba@cvc.uab.cat, adutta@cvc.uab.cat"
 
 import argparse
 import os
+import wget
 
-# Parse optios for downloading
-parser = argparse.ArgumentParser(description='Download dataset for Message Passing Algorithm.')
-parser.add_argument('datasets', metavar='D', type=str.lower, nargs='+', choices=['qm9'],
-                           help='Name of dataset to download [QM9]')
-parser.add_argument('-p', '--path', metavar='dir', type=str, nargs=1,
-        help='path to store the data (default ./)')
 
 # Download data from url
 def download_figshare(url, dir_path):
@@ -43,21 +39,40 @@ def download_qm9(data_dir):
     # dsC7O2H10nsd.xyz.tar.bz2
     download_data(url + '3195389', data_dir)
 
+# Download MUTAG and ENZYMES
+def download_mutag_enzymes():
+    wget.download(url, out=filename)
+
 # If not exists creates the specified folder
 def prepare_data_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
 if __name__ == '__main__':
+    
+    # Parse optios for downloading
+    parser = argparse.ArgumentParser(description='Download dataset for Message Passing Algorithm.')
+    # Positional arguments
+    parser.add_argument('datasets', metavar='D', type=str.lower, nargs='+', choices=['qm9'],
+                            help='Name of dataset to download [QM9]')
+    # I/O
+    parser.add_argument('-p', '--path', metavar='dir', type=str, nargs=1,
+                            help='path to store the data (default ./)')
+
     args = parser.parse_args()
+
+    # Check parameters
     if args.path is None:
         args.path = './'
 
+    # Init folder
     prepare_data_dir(args.path)
 
+    # Select datasets
     if 'qm9' in args.datasets:
         download_qm9(args.path)
     if 'mutag' in args.datasets:
         download_mutag(args.path)
     if 'enzymes' in args.datasets:
         download_enzymes(args.path)
+
--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from __future__ import print_function
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+    Trains a Neural Message Passing Model on various datasets. Methodologi defined in:
+
+    Gilmer, J., Schoenholz S.S., Riley, P.F., Vinyals, O., Dahl, G.E. (2017)
+    Neural Message Passing for Quantum Chemistry.
+    arXiv preprint arXiv:1704.01212 [cs.LG]
+
+"""
+
+__author__ = "Pau Riba, Anjan Dutta"
+__email__ = "priba@cvc.uab.cat, adutta@cvc.uab.cat"
 
 import argparse
-
 import torch
 
 ## Argument parser
-
 parser = argparse.ArgumentParser(description='Neural message passing')
 
 parser.add_argument('--dataset', default='MUTAG', help='MUTAG')
 parser.add_argument('--datasetPath', default='./db/mutag', help='dataset path')
+# Optimization Options
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='Input batch size for training (default: 64)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
@@ -20,6 +32,7 @@ parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='Learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                     help='SGD momentum (default: 0.5)')
+# i/o
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='How many batches to wait before logging training status')
 
