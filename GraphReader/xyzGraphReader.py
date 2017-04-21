058d893e10db5a708dee3d1194437f1d23c4b8a5
priba <pau.riba.fierrez@gmail.com>
2017-04-22 01:00:34 +0200

    Add Acceptor and Donor features

--- GraphReader/xyzGraphReader.py
+++ GraphReader/xyzGraphReader.py
@@ -10,8 +10,12 @@ Usage:
 
 import networkx as nx
 from rdkit import Chem
+from rdkit.Chem import ChemicalFeatures
+from rdkit import RDConfig
 from rdkit.Chem import Draw
 import numpy as np
+import os
+
 # from rdkit.Chem.inchi import MolFromInchi 
 
 __author__ = "Pau Riba, Anjan Dutta"
@@ -63,7 +67,7 @@ def xyzGraphReader(graph_file, verbose=False):
         # Atoms properties
         for i in range(na):
             atom_properties = f.readline()
-            g = addAtom(g, atom_properties) 
+        #    g = addAtom(g, atom_properties)
 
         # Frequencies
         f.readline()
@@ -76,25 +80,37 @@ def xyzGraphReader(graph_file, verbose=False):
         m = Chem.MolFromSmiles(smiles)
         print na
         print m.GetNumAtoms()
-        
-        for i in xrange(0,m.GetNumAtoms()):
+
+        fdefName = os.path.join(RDConfig.RDDataDir,'BaseFeatures.fdef')
+        factory = ChemicalFeatures.BuildFeatureFactory(fdefName)
+        feats = factory.GetFeaturesForMol(m)
+
+        # Create nodes
+        for i in xrange(0, m.GetNumAtoms()):
             atom_i = m.GetAtomWithIdx(i)
 
-            # Atom type
-            atom_i.GetSymbol()
-            # Atomic number
-            atom_i.GetAtomicNum()
+            # TODO Partial Charge
+            g.add_node(i, a_type=atom_i.GetSymbol(), a_num=atom_i.GetAtomicNum(), pc=0, acceptor=0, donor=0, aromatic=atom_i.GetIsAromatic(), hybridization=atom_i.GetHybridization(), num_h=atom_i.GetTotalNumHs())
 
-            # Aromatic
-            atom_i.GetIsAromatic()
-            # Hybridization
-            atom_i.GetHybridization()
+        for i in xrange(0, len(feats)):
+            if feats[i].GetFamily() == 'Donor':
+                node_list = feats[i].GetAtomIds()
+                for i in node_list:
+                    g.node[i]['donor'] = 1
+            elif feats[i].GetFamily() == 'Acceptor':
+                node_list = feats[i].GetAtomIds()
+                for i in node_list:
+                    g.node[i]['acceptor'] = 1
+
+        for i in xrange(0,m.GetNumAtoms()):
+            atom_i = m.GetAtomWithIdx(i)
 
             for j in xrange(0, m.GetNumAtoms()):
                 e_ij = m.GetBondBetweenAtoms(i, j)
                 if e_ij != None:
                     e_ij.GetBondType()
-
+                    # Donor?
+                    e_ij.GetBeginAtom()
                     # Acceptor?
                     e_ij.GetEndAtomIdx()
                     atom_j = m.GetAtomWithIdx(j)
