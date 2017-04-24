0301fbbb6e2b78d88cd14b1e1dcfd4d2e161beae
Pau Riba <priba@cvc.uab.cat>
2017-04-24 11:04:13 +0200

    Include hydrogens to xyzGraph reader

--- GraphReader/xyzGraphReader.py
+++ GraphReader/xyzGraphReader.py
@@ -8,20 +8,20 @@ Usage:
  
 """
 
+from __future__ import print_function
+
 import networkx as nx
 from rdkit import Chem
 from rdkit.Chem import ChemicalFeatures
 from rdkit import RDConfig
-from rdkit.Chem import Draw
 import numpy as np
 import os
-
-# from rdkit.Chem.inchi import MolFromInchi 
 
 __author__ = "Pau Riba, Anjan Dutta"
 __email__ = "priba@cvc.uab.cat, adutta@cvc.uab.cat"
 
-def initGraph(prop):
+
+def init_graph(prop):
     prop = prop.split()
     g_tag = prop[0]
     g_index = int(prop[1])
@@ -44,17 +44,8 @@ def initGraph(prop):
     return nx.Graph(tag=g_tag, index=g_index, A=g_A, B=g_B, C=g_C, mu=g_mu, alpha=g_alpha, homo=g_homo,
                     lumo=g_lumo, gap=g_gap, r2=g_r2, zpve=g_zpve, U0=g_U0, U=g_U, H=g_H, G=g_G, Cv=g_Cv)
 
-def addAtom(g, prop):
-    prop = prop.split()
-    a_type = prop[0]
-    a_coord = np.array(prop[1:4], dtype='|S4')
-    a_coord = a_coord.astype(np.float)
-    a_pc = float(prop[4])
-    g.add_node(len(g.nodes()), type=a_type, coord=a_coord, pc=a_pc)
-    return g
 
-
-def xyzGraphReader(graph_file, verbose=False):
+def xyz_graph_reader(graph_file, verbose=False):
 
     with open(graph_file,'r') as f:
         # Number of atoms
@@ -62,12 +53,14 @@ def xyzGraphReader(graph_file, verbose=False):
 
         # Graph properties
         properties = f.readline()
-        g = initGraph(properties)
-
+        g = init_graph(properties)
+        
+        atom_properties = []
         # Atoms properties
         for i in range(na):
-            atom_properties = f.readline()
-        #    g = addAtom(g, atom_properties)
+            a_properties = f.readline()
+            a_properties = a_properties.split()
+            atom_properties.append(a_properties)
 
         # Frequencies
         f.readline()
@@ -78,19 +71,20 @@ def xyzGraphReader(graph_file, verbose=False):
         smiles = smiles[0]
         
         m = Chem.MolFromSmiles(smiles)
-        print na
-        print m.GetNumAtoms()
+        m = Chem.AddHs(m)
 
-        fdefName = os.path.join(RDConfig.RDDataDir,'BaseFeatures.fdef')
-        factory = ChemicalFeatures.BuildFeatureFactory(fdefName)
+        fdef_name = os.path.join(RDConfig.RDDataDir, 'BaseFeatures.fdef')
+        factory = ChemicalFeatures.BuildFeatureFactory(fdef_name)
         feats = factory.GetFeaturesForMol(m)
 
         # Create nodes
         for i in xrange(0, m.GetNumAtoms()):
             atom_i = m.GetAtomWithIdx(i)
 
-            # TODO Partial Charge
-            g.add_node(i, a_type=atom_i.GetSymbol(), a_num=atom_i.GetAtomicNum(), pc=0, acceptor=0, donor=0, aromatic=atom_i.GetIsAromatic(), hybridization=atom_i.GetHybridization(), num_h=atom_i.GetTotalNumHs())
+            g.add_node(i, a_type=atom_i.GetSymbol(), a_num=atom_i.GetAtomicNum(), acceptor=0, donor=0,
+                       aromatic=atom_i.GetIsAromatic(), hybridization=atom_i.GetHybridization(),
+                       num_h=atom_i.GetTotalNumHs(), coord=np.array(atom_properties[i][1:4], dtype='|S4').astype(np.float),
+                       pc=float(atom_properties[i][4]))
 
         for i in xrange(0, len(feats)):
             if feats[i].GetFamily() == 'Donor':
@@ -102,37 +96,19 @@ def xyzGraphReader(graph_file, verbose=False):
                 for i in node_list:
                     g.node[i]['acceptor'] = 1
 
-        for i in xrange(0,m.GetNumAtoms()):
-            atom_i = m.GetAtomWithIdx(i)
-
+        # Read Edges
+        for i in xrange(0, m.GetNumAtoms()):
             for j in xrange(0, m.GetNumAtoms()):
                 e_ij = m.GetBondBetweenAtoms(i, j)
-                if e_ij != None:
-                    e_ij.GetBondType()
-                    # Donor?
-                    e_ij.GetBeginAtom()
-                    # Acceptor?
-                    e_ij.GetEndAtomIdx()
-                    atom_j = m.GetAtomWithIdx(j)
-                    if i not in g:
-                        g[i] = []
-                        g[i].append( (e_ij, j) )
+                if e_ij is not None:
+                    g.add_edge(i, j, b_type=e_ij.GetBondType(),
+                               distance=np.linalg.norm(g.node[i]['coord']-g.node[j]['coord']))
 
-        # InChI
-        inchis = f.readline()
-        inchis = inchis.split()
-        inchis = inchis[0]
-        print inchis
-        #m2 = MolFromInchi(inchis)
-
-        #print m2.GetNumAtoms()
-    if verbose:
-        Draw.MolToFile(m, 'test.png')
-    return g  
+    return g
     
 if __name__ == '__main__':
     
     #    graph_file = '../data/qm9/dsgdb9nsd/dsgdb9nsd_033462.xyz'
     
     graph_file = '../data/qm9/dsC702H10nsd/dsC7O2H10nsd_0001.xyz'
-    g = xyzGraphReader(graph_file)
+    g = xyz_graph_reader(graph_file)
