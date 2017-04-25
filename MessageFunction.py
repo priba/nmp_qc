fc1d63376e62beff200fb95ec986c39526d9a619
Pau Riba <priba@cvc.uab.cat>
2017-04-25 12:25:40 +0200

    Select Message function

--- MessageFunction.py
+++ MessageFunction.py
@@ -32,7 +32,17 @@ class MessageFunction:
         self.m_definition = lower(message_def)
 
         # TODO Check the correct funciton
-        self.m_definition = m_duvenaud
+        self.m_definition = {
+                    'duvenaud' : m_duvenaud
+                    'ggnn' : m_ggnn
+                    'intnet' : m_intnet
+                    'mgc' : m_mgc
+                    'bruna' : m_bruna
+                    'defferrard' : m_deff
+                    'kipf' : m_kipf
+                }.get(message_def, None)
+        if self.m_definition is None:
+            print('WARNING!: Message Function has not been set crorrectly\n\tIncorrect definition ' + message_def)
 
     # Get the name of the used message function
     def get_definition(self):
@@ -50,18 +60,37 @@ class MessageFunction:
         m = torch.mm(args.edge_mat(e_vw), torch.t(h_w))
         return m
 
-
     # Battaglia et al. (2016), Interaction Networks
-    
+    def m_intnet(self, h_v, h_w, e_vw, args):
+        # TODO 
+        m = [] 
+        return m
 
     # Kearnes et al. (2016), Molecular Graph Convolutions
-
+    def m_mgc(self, h_v, h_w, e_vw, args):
+        # TODO 
+        m = [] 
+        return m
+    
     # Laplacian based methods
     # Bruna et al. (2013)
+    def m_bruna(self, h_v, h_w, e_vw, args):
+        # TODO
+        m = [] 
+        return m
 
     # Defferrard et al. (2016)
+    def m_deff(self, h_v, h_w, e_vw, args):
+        # TODO
+        m = []
+        return m
 
     # Kipf & Welling (2016)
+    def m_kipf(self, h_v, h_w, e_vw, args):
+        # TODO
+        m = []
+        return m
+
 
 if __name__ == '__main__':
 
