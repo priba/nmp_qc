1d34a23f7484eaafa51b9212fcf0bdb4b4b62636
AnjanDutta <duttanjan@gmail.com>
2017-05-03 16:56:29 +0200

    function average_error_ratio is included

--- LogMetric.py
+++ LogMetric.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 __author__ = "Pau Riba, Anjan Dutta"
 __email__ = "priba@cvc.uab.cat, adutta@cvc.uab.cat"
 
+def average_error_ratio(pred, target):
+    if type(pred) is not np.ndarray:
+        pred = np.array(pred)
+    if type(target) is not np.ndarray:
+        target = np.array(target)       
+        
+    return np.mean(np.divide(np.abs(pred - target), target))
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
